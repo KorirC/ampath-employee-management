0457575e301d176ed66325861315b066fcd36782
Maureen Nduta <67265839+maureennduta@users.noreply.github.com>
2021-09-22 09:57:14 +0300

    Login and Register Components. (#8)
    * Login and Register Components.
    
    * Update register.test.tsx
    
    * Update register.test.tsx
    
    * Linting Updates
    
    * fix failing test
    
    Co-authored-by: Donald Kibet <chelashawdonald@yahoo.com>

---
+++ front-end/src/tests/login.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from '../Components/Login/login';
+
+describe('Login render Page', () => {
+  it('renders the input fields and login button', () => {
+    const { getByTestId } = render(<Login />);
+    expect(getByTestId('username')).toBeInTheDocument();
+    expect(getByTestId('password')).toBeInTheDocument();
+    expect(getByTestId('login')).toBeInTheDocument();
+  });
+});
